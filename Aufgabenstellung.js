69d60b5a10e328a9f472db26cd00c23422a1b3d2
Hi2272 <hille_r@gmx.de>
2022-02-18 19:29:53 +0100

    dasda

--- Aufgabenstellung.js
+++ Aufgabenstellung.js
@@ -71,8 +71,9 @@ function anzeigen() {
         txt = "Aufgabe " + (nummer + 1).toString() + "/" + angabe.length + ": " + angabe[nummer].txt;
     }
     document.getElementById("Angabe").innerHTML = txt;
-    document.getElementById("Bild").innerHTML = '<img src="'+href+'//screenshot_' + s + '.png" ' +
-        'height=' + (height / 2).toString() + '>';
+    pfad='<img src="'+href+'//screenshot_' + s + '.png" ' +
+    'height=' + (height / 2).toString() + '>';
+    document.getElementById("Bild").innerHTML = pfad;
     zeit = Date.now();
 
 }
