aa36ee20933ba04b2ae5522a9ca386890697ea16
Hi2272 <hille_r@gmx.de>
2023-02-14 10:20:02 +0100

    Verzeichnisfehler behoben

--- AufgabenstellungMitBlueJ.js
+++ AufgabenstellungMitBlueJ.js
@@ -22,7 +22,7 @@ xmlhttp.onload = function () {
     document.getElementById("quelle").innerHTML = "Bildquelle: " + jsonData.copyright +
         "<br>2022 Rainer Hille <br> Unter Verwwendung von  <a href='https://www.cssscript.com/toast-style-web-notifications-in-vanilla-javascript-vanillatoasts/'>VanillaToasts.js</a>";
     var s="Klicken um die Vorlage herunterzuladen ⇒ Rechtsklick auf heruntergeladene Datei ⇒ Öffnen mit BlueJ<br>"+
-    "<a href=\"https://Hi2272.github.io/00Informatik/AufgabenstellungRing/"+jsonData.blueJ+"\">BlueJ Vorlage</a>";
+    "<a href=\"https://Hi2272.github.io/00Informatik/"+jsonData.blueJ+"\">BlueJ Vorlage</a>";
 
         document.getElementById("BlueJ").innerHTML=s;
     /* 
