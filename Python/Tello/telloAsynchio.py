e682a89c41043eb3124c5edea02eab8d087703ac
Rainer Hille <hille_r@gmx.de>
2024-02-20 08:16:34 +0100

    Drohne

---
+++ Python/Tello/telloAsynchio.py
@@ -0,0 +1,14 @@
+import asyncio
+from tello_asyncio import Tello
+
+async def main():
+    drone = Tello()
+    try:
+        await drone.connect()
+        await drone.takeoff()
+        await drone.turn_clockwise(360)
+        await drone.land()
+    finally:
+        await drone.disconnect()
+
+asyncio.run(main())
