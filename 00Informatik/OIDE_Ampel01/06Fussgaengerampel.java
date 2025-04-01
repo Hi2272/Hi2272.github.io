49cf2c2de49d9bfc4823bc347aeb9d38bfe0d4b6
Rainer Hille <hille_r@gmx.de>
2025-04-01 08:49:57 +0200

    Ampeln

--- 00Informatik/OIDE_Ampel01/06Fussgaengerampel.java
+++ 00Informatik/OIDE_Ampel01/06Fussgaengerampel.java
@@ -2,9 +2,7 @@ public class Fussgaengerampel extends Ampel {
 
    public Fussgaengerampel(int x, int y, int b, int h, Color[] farbe) {
       super(x, y, b, h, farbe);
-      for (int i = 0; i < lampe.length; i++) {
-         lampe[i] = new Lampe(x + b / 2, y + h / 4 + i * h / 2, h / 5, farbe[i]);
-      }
+     
       lampe[0].an();
    }
  public void rot() {
--- 00Informatik/OIDE_Ampel01/06Verkehrsampel.java
+++ 00Informatik/OIDE_Ampel01/06Verkehrsampel.java
@@ -2,9 +2,7 @@ public class Verkehrsampel extends Ampel {
 
    public Verkehrsampel(int x, int y, int b, int h, Color[] farbe) {
       super(x, y, b, h, farbe);
-      for (int i = 0; i < lampe.length; i++) {
-         lampe[i] = new Lampe(x + b / 2, y + h / 6 + i * h / 3, h / 8, farbe[i]);
-      }
+   
       lampe[0].an();
    }
 
