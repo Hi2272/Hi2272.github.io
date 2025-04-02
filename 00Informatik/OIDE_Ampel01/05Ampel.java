e5926d0d630ac5da40f73339434744e748353701
Rainer Hille <hille_r@gmx.de>
2025-04-02 12:17:06 +0200

    Übersicht Felder

--- 00Informatik/OIDE_Ampel01/05Ampel.java
+++ 00Informatik/OIDE_Ampel01/05Ampel.java
@@ -1,9 +1,9 @@
-public abstract class Ampel extends RoundedRectangle {
+public abstract class Ampel extends Rectangle {
    
    protected Lampe[] lampe;
    
    public Ampel(int x, int y, int h, int b, Color[] farbe) {
-      super(x, y, h, b, 10);
+      super(x,y,h,b);
       setFillColor(Color.darkcyan);
       this.lampe = new Lampe[farbe.length];
    }
@@ -17,10 +17,23 @@ public abstract class Ampel extends RoundedRectangle {
    public void schalten(int i) {
       allesAus();
       lampe[i].an();
+      SystemTools.pause(1000);
    }
 
-   public void rot() {
+    public void rot() {
       schalten(0);
+   }
+   
+   public void wirdGruen(){
+      for (int i=0;i<lampe.length;i++){
+         schalten(i);
+      }
+   }
+
+   public void wirdRot(){
+      for (int i=lampe.length-1;i>=0;i--){
+         schalten(i);
+      }
    }
    
 }
