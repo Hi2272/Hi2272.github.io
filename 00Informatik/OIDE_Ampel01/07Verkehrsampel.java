2fd0626048b16dee05be9c74c2f06c000d24eb13
Rainer Hille <hille_r@gmx.de>
2025-04-04 09:31:06 +0200

    Ampeln repariert

--- 00Informatik/OIDE_Ampel01/07Verkehrsampel.java
+++ 00Informatik/OIDE_Ampel01/07Verkehrsampel.java
@@ -2,7 +2,9 @@ public class Verkehrsampel extends Ampel {
 
    public Verkehrsampel(int x, int y, int b, int h, Color[] farbe) {
       super(x, y, b, h, farbe);
-     
+        for (int i = 0; i < lampe.length; i++) {
+         lampe[i] = new Lampe(x + b / 2, y + h / 6 + i * h / 3, h / 8, farbe[i]);
+      }
       gruen();
    }
 
