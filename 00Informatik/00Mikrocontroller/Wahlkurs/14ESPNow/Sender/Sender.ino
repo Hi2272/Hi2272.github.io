72f5e739e9aeffe6878272e4f346631d22aae102
Hi2272 <hille_r@gmx.de>
2025-07-12 06:28:22 +0200

    Sender

--- 00Informatik/00Mikrocontroller/Wahlkurs/14ESPNow/Sender/Sender.ino
+++ 00Informatik/00Mikrocontroller/Wahlkurs/14ESPNow/Sender/Sender.ino
@@ -1,49 +1,45 @@
-#include <esp_now.h>  // Einbinden der ESP-NOW-Bibliothek
-#include <WiFi.h>     // Einbinden der WiFi-Bibliothek
+#include <AccelStepper.h>
 
-// MAC-Adresse des Empfängers
-uint8_t empfaengerMAC[] = {0xC8, 0xF0, 0x9E, 0x74, 0x16, 0xC4};
-
-int nr=0;  // Zahl, die gesendet werden soll
-
-esp_now_peer_info_t peerInfo;  // Struktur für Peer-Informationen
-
+const byte Fullstep = 4;
+const byte Halfstep = 8;
+const short fullRevolution = 2038;
+const float SteppDegree = 11.32;  // Half/Full 11.32 / 5.66
+// Pins IN1-IN3-IN2-IN4
+AccelStepper stepper(Halfstep, 11, 9, 10, 8);
 void setup() {
-  Serial.begin(115200);  // Start der seriellen Kommunikation mit 115200 Baud
+  stepper.setMaxSpeed(250.0);
+  stepper.setAcceleration(100.0);
+  Serial.begin(9600);
 
-  WiFi.mode(WIFI_STA);  // Setzen des WiFi-Modus auf Station, d.h. der ESP wirkt als Client im Netzwerk
+  blattEinziehen();
+  delay(2000);
 
-  // Initialisieren von ESP-NOW und Überprüfen auf Fehler
-
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Fehler bei der Initialisierung von ESP-NOW");
-    return;  // Hier bricht die Ausführung ab.
+  for (int i = 0; i < 3; i++) {
+    zeileEinziehen(1);
+    delay(1000);
   }
-  
-  // Kopieren der Empfänger-Adresse in die Variable peerInfo
-  memcpy(peerInfo.peer_addr, empfaengerMAC, 6);
-  peerInfo.channel = 0;  // Standard-Kanal zum Senden
-  peerInfo.encrypt = false;  // Keine Verschlüsselung verwenden
-  
-  // Hinzufügen des Peers zur ESP-NOW-Liste und Überprüfen auf Fehler
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Fehler beim Hinzufügen des Peers");
-    return;
-  }
+  blattAuswerfen();
 }
 
+void einziehen(float wert) {
+  Serial.print(stepper.currentPosition());
+  stepper.runToNewPosition(stepper.currentPosition()-1 * wert);  // Cause an overshoot then back to 0
+  Serial.println("->"+String(stepper.currentPosition()));
+}
+
+void blattEinziehen() {
+  stepper.setCurrentPosition(0);
+  einziehen(800);
+}
+
+void zeileEinziehen(float abstand) {
+  einziehen(abstand * 80);
+}
+
+void blattAuswerfen() {
+  stepper.runToNewPosition(-5000);
+}
+
+
 void loop() {
-  nr++;
-  // Senden der Nachricht über ESP-NOW
-  esp_err_t ergebnis = esp_now_send(empfaengerMAC, (uint8_t *) &nr, sizeof(nr));
-   
-  // Überprüfen, ob das Senden erfolgreich war und entsprechende Meldung ausgeben
-  if (ergebnis == ESP_OK) {
-    Serial.println(String(nr)+" gesendet!");
-  }
-  else {
-    Serial.println("Fehler beim Senden von "+String(nr));
-  }
-  
-  delay(2000);  // Verzögerung von 2 Sekunden vor dem nächsten Senden
 }
