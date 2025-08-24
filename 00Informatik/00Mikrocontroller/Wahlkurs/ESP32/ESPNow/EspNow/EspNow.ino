04953bda2303c676da3e52e9f8b2e3d5b3cbf4a7
Hi2272 <hille_r@gmx.de>
2025-08-24 07:19:45 +0200

    ESPNOW

---
+++ 00Informatik/00Mikrocontroller/Wahlkurs/ESP32/ESPNow/EspNow/EspNow.ino
@@ -0,0 +1,37 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+int empfangeneZahl;  // Variable zum Speichern des empfangenen Ganzzahlwerts
+
+// Callback-Funktion, die bei Empfang von Daten aufgerufen wird
+void datenEmpfangen(const uint8_t * mac, const uint8_t *eingehendeDaten, int len) {
+  // Überprüfen, ob die Länge der empfangenen Daten der Größe von empfangeneZahl entspricht
+  if (len == sizeof(empfangeneZahl)) {  
+    memcpy(&empfangeneZahl, eingehendeDaten, sizeof(empfangeneZahl));  // Kopiere die empfangenen Daten in die Variable
+    Serial.print("Bytes empfangen: ");  // Ausgabe der Anzahl empfangener Bytes
+    Serial.println(len);
+    Serial.print("Empfangene Int-Zahl: ");  // Ausgabe der empfangenen Ganzzahl
+    Serial.println(empfangeneZahl);
+    Serial.println();
+  } else {
+    Serial.println("Fehler: Falsche Datenlänge empfangen");  // Fehlermeldung bei falscher Datenlänge
+  }
+}
+ 
+void setup() {
+  Serial.begin(115200);  // Start der seriellen Kommunikation mit 115200 Baud
+  WiFi.mode(WIFI_STA);  // Setzen des WiFi-Modus auf Station
+
+  // Initialisieren von ESP-NOW und Überprüfen auf Fehler
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Fehler bei der Initialisierung von ESP-NOW");
+    return;
+  }
+  
+  // Registrieren der Callback-Funktion für empfangene Daten
+  esp_now_register_recv_cb(esp_now_recv_cb_t(datenEmpfangen));
+}
+ 
+void loop() {
+  // Hier ist kein Code nötig, da die Datenverarbeitung in der Callback-Funktion erfolgt. 
+}
